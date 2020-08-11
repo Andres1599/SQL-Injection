727eb88a4d13818fd5d6a2c17e9a23369c716c11
Andres Higueros <higueros71@gmail.com>
2020-08-10 18:35:03 -0600

    add: agregando cambios en el query

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -3,13 +3,17 @@ import Singleton from '../models/singleton.model.js'
 const conn = new Singleton().getInstance().conn();
 
 function getUsers(req, res) {
-    conn.query('SELECT * FROM users', (err, rows = []) => {
-        if (err) {
-            res.status(404).json({
-                err,
-                message: 'Error'
-            });
+    conn.promise().query('SELECT * FROM users').then((rows) => {
+        if (rows) {
+            res.status(200).json({
+                users: rows[0]
+            })
         }
+    }).catch(err => {
+        res.status(404).json({
+            err,
+            message: 'Error'
+        });
     })
 }
 
@@ -22,22 +26,21 @@ function loginUser(req, res) {
             message: 'Error, send credentials'
         });
     }
-    conn.query('SELECT * FROM `users` WHERE `email` = ? and `password` = ?', [userCredential.email, userCredential.password], (err, rows) => {
-        if (err) {
-            res.status(404).json({
-                err,
-                message: 'Error'
+
+    const statement = 'SELECT * FROM users WHERE email=\'' + `${userCredential.email}` + '\' and password=\'' + `${userCredential.password}` + '\'';
+    
+    conn.promise().query(statement).then(
+        (rows, fields) => {
+            res.status(200).json({
+                user: rows[0],
+                query: statement
             });
         }
-
-        if (rows.length === 0) {
-            res.status(400).json({
-                message: 'invalid credentials'
-            });
-        }
-
-        res.status(200).json({
-            user: rows[0]
+    ).catch(err => {
+        res.status(404).json({
+            err,
+            message: 'Error',
+            query: statement
         });
     });
 }
