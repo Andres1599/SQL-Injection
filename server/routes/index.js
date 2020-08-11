078825903f12740b58b34058d2eda65a4866414c
Andres Higueros <higueros71@gmail.com>
2020-08-11 11:37:29 -0600

    add: agregando rutas para poder crear, eliminar e insertar un usuario

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,11 @@ import { getUsers, loginUser } from '../controllers/user.controller.js'
 
 let router = express.Router();
 
+/* user routes */
 router.get('/users', getUsers);
 router.post('/user/login', loginUser);
+router.post('/user/', loginUser);
+router.put('/user/', loginUser);
+router.delete('/user/', loginUser);
 
 export default router;
